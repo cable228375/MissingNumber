ad305d741b8949db8367905254b1b1ebb89ba217
cable228375 <calbbromeo@gmail.com>
2025-04-24 00:42:50 +1000

    Added operation.

--- MissingNumber/Program.cs
+++ MissingNumber/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
-
 class MissingNumberCalculator
 {
     public static void PrintArray(int[] arr)
@@ -12,9 +11,26 @@ class MissingNumberCalculator
 
     public static int DoOperation(int[] arr)
     {
-        PrintArray(arr);
+        // Quicksort array (ascending order)
+        Array.Sort(arr);
+
+        // Start at 0 (assumed to always be there)
         int result = 0;
-        return result;
+        foreach (int i in arr)
+        {
+            // Check if the current integer exists
+            if (result == i)
+            {
+                // If it does, check for the next one
+                result++;
+            }
+            else
+            {
+                // If it doesn't exist, return the missing integer
+                return result;
+            }
+        }
+        return 0;
     }
 }
 
@@ -22,24 +38,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Display title as the C# console calculator app.
         Console.WriteLine("Console Missing Number Calculator in C#\r");
         Console.WriteLine("------------------------\n");
 
         string? input = "";
         int result = 0;
 
-        Console.WriteLine("Please enter the array of n integers in the format: \"[a, b, ..., c]\"");
+        Console.WriteLine("Please enter an array of n integers in the format: \"[a, b, ..., c]\"");
         input = Console.ReadLine();
 
-        // Do some better error handling here before processing.
+        // Do some better error handling here before processing
         int[] arr;
         if (input != null)
         {
             arr = Array.ConvertAll(input.Trim('[', ']', ' ').Split(','), Convert.ToInt32);
             result = MissingNumberCalculator.DoOperation(arr);
 
-            Console.WriteLine("This missing number is: " +  result);
+            Console.WriteLine(result);
         }
         return;
     }
